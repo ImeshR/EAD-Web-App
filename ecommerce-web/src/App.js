2a01d818cc6912684ddd5a7c24c95179dde6d979
Eshan Imesh <111144332+ImeshR@users.noreply.github.com>
2024-09-28 00:56:19 +0530

    initial

---
+++ ecommerce-web/src/App.js
@@ -0,0 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        {/* Routes go here */}
+        {/* <Route path="/" element={<Home />} /> */}
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
